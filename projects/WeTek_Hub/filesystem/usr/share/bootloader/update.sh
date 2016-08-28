740df74409d334f91a5a609091548a9cc7c024bb
Jamie Coldhill <wrxtasy@amnet.net.au>
2016-08-28 10:24:16 +0800

    dtb: use linux device tree blob

--- projects/WeTek_Hub/filesystem/usr/share/bootloader/update.sh
+++ projects/WeTek_Hub/filesystem/usr/share/bootloader/update.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 
 ################################################################################
-#  This program is free software: you can redistribute it and/or modify
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 [ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
 [ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
 
+IMAGE_KERNEL="/flash/kernel.img"
+
+# parse command line arguments
+for arg in $(cat /proc/cmdline); do
+  case $arg in
+    BOOT_IMAGE=*)
+      IMAGE_KERNEL="${arg#*=}"
+    ;;
+  esac
+done
+
+
 # mount $BOOT_ROOT r/w
   mount -o remount,rw $BOOT_ROOT
 
-# update Device Tree Blobs
-  for dtb in wetek_hub.dtb dtb.img; do
-    if [ -f $SYSTEM_ROOT/usr/share/bootloader/$dtb ]; then
-      echo "*** updating Device Tree Blob: $dtb ..."
-      cp -p $SYSTEM_ROOT/usr/share/bootloader/$dtb $BOOT_ROOT/dtb.img
-    fi
-  done
+if [ -b $IMAGE_KERNEL ]; then
+  dd if="$SYSTEM_ROOT/usr/share/bootloader/dtb.img" of="/dev/dtb" bs=262144 2>&1
+else
+  cp -p $SYSTEM_ROOT/usr/share/bootloader/dtb.img $BOOT_ROOT
+fi
 
 # mount $BOOT_ROOT r/o
   sync
