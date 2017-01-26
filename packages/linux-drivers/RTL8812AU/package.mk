738820e945f9aa36e2805a79e1f8afb31742c29d
Jamie Coldhill <wrxtasy@amnet.net.au>
2017-01-26 11:48:56 +0800

    RTL8812AU: update to v4.3.20

--- packages/linux-drivers/RTL8812AU/package.mk
+++ packages/linux-drivers/RTL8812AU/package.mk
@@ -17,16 +17,15 @@
 ################################################################################
 
 PKG_NAME="RTL8812AU"
-PKG_VERSION="928e27f"
+PKG_VERSION="4.3.20"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/Grawp/rtl8812au_rtl8821au"
 PKG_URL="https://github.com/Grawp/rtl8812au_rtl8821au/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="rtl8812au_rtl8821au-$PKG_VERSION*"
+PKG_SOURCE_DIR="rtl8812au_rtl8821au-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_PRIORITY="optional"
 PKG_SECTION="driver"
 PKG_SHORTDESC="Realtek RTL8812AU Linux 3.x driver"
 PKG_LONGDESC="Realtek RTL8812AU Linux 3.x driver"
