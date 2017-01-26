ba51e2f88da1097d888ad9cc5d6942d4b8d4d4e7
Jamie Coldhill <wrxtasy@amnet.net.au>
2017-01-26 11:04:05 +0800

    lirc: add switch for disabling via LE settings

--- packages/sysutils/lirc/package.mk
+++ packages/sysutils/lirc/package.mk
@@ -56,12 +56,14 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/pronto2lirc
 
   mkdir -p $INSTALL/etc/lirc
-    cp $PKG_DIR/config/lircd.conf.xbox $INSTALL/etc/lirc
-    cp $PKG_DIR/config/lircd.conf.rpi $INSTALL/etc/lirc
+    cp $PKG_DIR/config/lircd.conf.* $INSTALL/etc/lirc
 
   mkdir -p $INSTALL/usr/lib/libreelec
     cp $PKG_DIR/scripts/lircd_helper $INSTALL/usr/lib/libreelec
 
   mkdir -p $INSTALL/usr/lib/udev
     cp $PKG_DIR/scripts/lircd_wakeup_enable $INSTALL/usr/lib/udev
+
+  mkdir -p $INSTALL/usr/share/services
+    cp -P $PKG_DIR/default.d/*.conf $INSTALL/usr/share/services
 }
